03caee596335e731db61e9bb280298213fc16314
Alessandro Franceschi <al@lab42.it>
2013-02-16 17:55:36 +0100

    Added logrotate::conf and logrotate::file

---
+++ manifests/conf.pp
@@ -0,0 +1,56 @@
+# Define: logrotate::file
+#
+# This define has beed adapted from:
+# https://github.com/arioch/puppet-logrotate/blob/master/manifests/file.pp
+#
+# [*log*]
+#   Path(s) of the log(s) to refer into the logrotate file.
+#   Can be an array. Required.
+#
+# [*options*]
+#   Logrotate options for this log. Must be an array.
+#   Default: [ 'weekly', 'compress', 'rotate 7', 'missingok' ],
+#
+# [*prerotate*]
+#   Optional prerotate script.
+#
+# [*postrotate*]
+#   Optional postrotate script.
+#
+# [*template*]
+#   Sets an alternative template to use as content for conf file
+#   Default: logrotate/conf.erb
+#
+# [*ensure*]
+#   Defines if the file has to be present or absent. Default: present
+#   Note that if you set absent => true on the main logrotate class
+#   this value is automatically forced to absent
+#
+define logrotate::conf (
+  $log,
+  $options    = [ 'weekly', 'compress', 'rotate 7', 'missingok' ],
+  $prerotate  = 'NONE',
+  $postrotate = 'NONE',
+  $template   = 'logrotate/conf.erb',
+  $ensure     = present ) {
+
+  require logrotate
+
+  $real_ensure = $logrotate::bool_absent ? {
+    true  => 'absent'
+    false => $ensure,
+  }
+
+  file { "logrotate_conf_${name}":
+    ensure  => $real_ensure,
+    path    => "${logrotate::config_dir}/${name}",
+    mode    => $logrotate::config_file_mode,
+    owner   => $logrotate::config_file_owner,
+    group   => $logrotate::config_file_group,
+    require => Package['logrotate'],
+    content => template($template),
+    audit   => $logrotate::manage_audit,
+    noops   => $logrotate::noops,
+  }
+
+}
---
+++ manifests/file.pp
@@ -0,0 +1,61 @@
+# = Define: logrotate::file
+#
+# This defines allows the addition of a single configuration file
+# in logrotate.d with the contante you want
+# You can use this as an alternative to logrotate::conf and logrotate::rule
+#
+# == Parameters
+#
+# [*source*]
+#   Sets the content of source parameter for the conf file
+#   If defined, logrotate conf file will have the param: source => $source
+#
+# [*template*]
+#   Sets the path to the template to use as content for conf file
+#   If defined, logrotate conf file has: content => content("$template")
+#   Note source and template parameters are mutually exclusive: don't use both
+#
+# [*ensure*]
+#   Defines if the file has to be present or absent. Default: present
+#
+# == Usage
+# logrotate::file { "apache": source => 'puppet:///modules/site/logrotate/apache' }
+# or
+# logrotate::file { "apache": content => 'template('site/logrotate/apache')' }
+#
+define logrotate::file (
+  $source  = '' ,
+  $content = '' ,
+  $ensure  = present ) {
+
+  require logrotate
+
+  $manage_file_source = $source ? {
+    ''        => undef,
+    default   => $source,
+  }
+
+  $manage_file_content = $content ? {
+    ''        => undef,
+    default   => $content,
+  }
+
+  $real_ensure = $logrotate::bool_absent ? {
+    true  => 'absent'
+    false => $ensure,
+  }
+
+  file { "logrotate_file_$name":
+    ensure  => $real_ensure,
+    path    => "${logrotate::config_dir}/${name}",
+    mode    => $logrotate::config_file_mode,
+    owner   => $logrotate::config_file_owner,
+    group   => $logrotate::config_file_group,
+    require => Package['logrotate'],
+    source  => $manage_file_source,
+    content => $manage_file_content,
+    audit   => $logrotate::manage_audit,
+    noops   => $logrotate::noops,
+  }
+
+}
